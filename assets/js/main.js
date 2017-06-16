24a33269085da40725309d2cafff623410423612
Paula Ponce <paula.poncez@usach.cl>
2017-06-15 21:21:39 -0400

    validation

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+var btnInicio = document.getElementById("inicio-sesion");
+btnInicio.addEventListener("click", validateForm);
+
+function validateForm(){
+	var nombre = document.getElementById("name").value;
+	var pass = document.getElementById("password").value;
+
+	var valNombre = document.getElementsByClassName("input-group")[0];
+	var valPass = document.getElementsByClassName("input-group")[1];
+
+	if(nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)){
+		var nombreSpan = document.createElement("span");
+		var nombreText = document.createTextNode("Nombre de usuario incorrecto");
+		nombreSpan.appendChild(nombreText);
+		valNombre.appendChild(nombreSpan);
+	}
+	if((pass == "") || (pass == 123456) || (pass.length <= 6)){
+		var passSpan = document.createElement("span");
+		var passText = document.createTextNode("Contraseña incorrecta");
+		passSpan.appendChild(passText);
+		valPass.appendChild(passSpan);
+	}
+
+	clear(); //resetear campos 
+}
+
+function clear(){
+	document.getElementById("name").value = "";
+	document.getElementById("password").value = "";
+}
